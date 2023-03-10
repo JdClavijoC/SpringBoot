4b82806db4d7bb1936edfa4e36b155c32e420c53
JdClavijoC <jdclavijoc@ufpso.edu.co>
2023-03-09 20:21:11 -0500

    Create User controller

---
+++ main/java/com/procesos/concesionario/controllers/UserController.java
@@ -0,0 +1,20 @@
+package com.procesos.concesionario.controllers;
+
+import com.procesos.concesionario.models.User;
+import com.procesos.concesionario.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController
+{
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = "/user/{id}")
+    public User getById(Long id)
+    {
+        return userService.getUserById(id).get();
+    }
+}

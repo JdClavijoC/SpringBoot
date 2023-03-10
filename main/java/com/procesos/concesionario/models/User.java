2462d22e80d4832f9946b6fec467b2d8d7d3851e
JdClavijoC <jdclavijoc@ufpso.edu.co>
2023-03-09 20:03:53 -0500

    Create user repository

--- main/java/com/procesos/concesionario/models/User.java
+++ main/java/com/procesos/concesionario/models/User.java
@@ -1,10 +1,20 @@
 package com.procesos.concesionario.models;
 
+import lombok.Data;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
+@Data
+@Entity
 public class User {
+
+    @Id
     private Long _id;
+    @Column(name = "first_name")
     private String _firstName;
+    @Column(name = "last_name")
     private String _lastName;
     private String _email;
     private String _password;

72475ee65189cc7b17874e82f28a5c6cf0b29363
JdClavijoC <jdclavijoc@ufpso.edu.co>
2023-03-09 19:25:08 -0500

    User Create /model

---
+++ main/java/com/procesos/concesionario/models/User.java
@@ -0,0 +1,13 @@
+package com.procesos.concesionario.models;
+
+import java.time.LocalDate;
+
+public class User {
+    private Long _id;
+    private String _firstName;
+    private String _lastName;
+    private String _email;
+    private String _password;
+    private String _address;
+    private LocalDate _birthday;
+}
